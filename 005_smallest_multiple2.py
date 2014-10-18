4b93de8a56215c7e778b112d016f79274144672e
Paul Foster <paulfstr@gmail.com>
2014-10-18 14:57:18 +1100

    Tidy up and add comments to problem 005v2

--- 005_smallest_multiple2.py
+++ 005_smallest_multiple2.py
@@ -1,4 +1,3 @@
-import itertools
 from functools import reduce
 from operator import mul
 
@@ -6,26 +5,6 @@ from helpers import primes
 
 RANGE_MAX = 21
 
-
-# This function is unused
-def evenly_divisible(num):
-    return all(num % x == 0 for x in range(1, RANGE_MAX))
-
-
-# This function is unused
-def find_unsatisfied_divisors(num):
-    return [x for x in range(1, RANGE_MAX) if num % x != 0]
-
-
-# Note
-# f([1, 2, 2, 3], [1, 2, 3]) -> True
-# f([1, 2, 2, 3], [1, 1]) -> False
-def contains_all_elements(original, elements):
-    for element in elements:
-        if not element in original:
-            return False
-        original.remove(element)
-    return True
 
 def main():
     # Find the prime factors of every number from 1 to 20
@@ -35,25 +14,18 @@ def main():
 
     for key, value in prime_factors.items():
         answer_factors_copy = answer_factors[:]
+
         for factor in value:
-#            print(answer_factors_copy)
-#            print(factor)
+            # If we don't already have this factor
             if not factor in answer_factors_copy:
-                answer_factors.append(factor)
+                answer_factors.append(factor) # Store it
             else:
+                # Remove the factor from the copy to ensure we keep repeat factors
                 answer_factors_copy.remove(factor)
 
-#        if contains_all_elements(answer_factors, value):
-#        print(key, value)
-
-    print(answer_factors)
-
+    # Multiply the factors together to get the answer
     answer = reduce(mul, answer_factors, 1)
     print(answer)
-
-
-#    print(contains_all_elements([1, 2, 2, 3], [1, 2, 3]))
-#    print(contains_all_elements([1, 2, 2, 3], [1, 1]))
 
 
 if __name__ == '__main__':
