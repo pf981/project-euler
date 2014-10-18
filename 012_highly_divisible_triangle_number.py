8fce40991fcf901dec22b08fb6f58663416eaaf1
Paul Foster <paulfstr@gmail.com>
2014-10-18 21:22:39 +1100

    Complete problem 012

--- 012_highly_divisible_triangle_number.py
+++ 012_highly_divisible_triangle_number.py
@@ -1,15 +1,19 @@
 import itertools
 from helpers import helpers
 
+MIN_DIVISORS = 500
+
 def triangle_numbers():
+    """Generates the first million triangle numbers"""
     triangle_num = 0
-    for i in range(1, 100):
+    for i in range(1, 1000000):
         triangle_num += i
         yield triangle_num
 
 def main():
     for triangle_num in triangle_numbers():
-        if len(helpers.factors(triangle_num)) > 5:
+        # If it has enough divisors to be the solution
+        if len(helpers.factors(triangle_num)) > MIN_DIVISORS:
             answer = triangle_num
             break
 
