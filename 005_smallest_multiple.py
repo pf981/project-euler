3322e188bd76cc553190f1f24025745daced96ab
Paul Foster <paulfstr@gmail.com>
2014-10-18 14:29:16 +1100

    Tidy code and add comments for problem 005

--- 005_smallest_multiple.py
+++ 005_smallest_multiple.py
@@ -1,53 +1,44 @@
 import itertools
 from functools import reduce
 from operator import mul
-#from math import pow
 
 from helpers import primes
 
 RANGE_MAX = 11
 
+
+# This function is unused
 def evenly_divisible(num):
     return all(num % x == 0 for x in range(1, RANGE_MAX))
 
+
+# This function is unused
 def find_unsatisfied_divisors(num):
     return [x for x in range(1, RANGE_MAX) if num % x != 0]
 
+
 def main():
+    # Find the prime factors of every number from 1 to 20
     prime_factors = primes.prime_factors_range(RANGE_MAX)
-    print(prime_factors)
 
+    # Find the unique list of prime factors
     unique_factors = set(itertools.chain.from_iterable(prime_factors.values()))
-    print(unique_factors)
 
+    # Count how many times each prime occurs in each number
     factors_counts = {}
     for factor in unique_factors:
         number_with_most_repeats_of_factor = max(prime_factors, key=lambda p: prime_factors[p].count(factor))
         factors_counts[factor] = prime_factors[number_with_most_repeats_of_factor].count(factor)
 
-    print(factors_counts)
-
-
+    # Determine the number with the most occurances of each prime
+    # Calculate factor^count_of_that_factor
     all_factors_of_answer = [pow(factor, factors_counts[factor]) for factor in factors_counts]
-    print(all_factors_of_answer)
+
+    # Multiply all these factors together to get the answer
     answer = reduce(mul, all_factors_of_answer, 1)
 
     print(answer)
 
-#    for vals in p.factors.values():
-#    for vals in p.factors:
-#        print(vals)
-#
-#    print(p.factors)
-##    l = list(p.factors.values())
-#
-#    s = set(itertools.chain.from_iterable(l))
-##    s = list(itertools.chain.from_iterable(l))
-#    print(reduce(mul, s, 1)*2*2*3)
-#    print(find_unsatisfied_divisors(reduce(mul, s, 1)))
-##    next(x for x in range())
-##    print(evenly_divisible(2520))
-##    print(evenly_divisible(2521))
 
 if __name__ == '__main__':
   main()
