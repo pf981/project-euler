04eac50aaa8939dc438cae932a17fada5c558b8c
Paul Foster <paulfstr@gmail.com>
2014-10-19 21:24:09 +1100

    Complete problem 35

--- 035_circular_primes.py
+++ 035_circular_primes.py
@@ -8,38 +8,17 @@ def rotations(n):
     string = str(n)
     for i in range(len(string)):
         yield int(string[i:] + string[:i])
+        # or: string.append(string.pop(0))
 
 def is_circular(prime):
-#    return all(int(rotation) in PRIMES for rotation in permutations(str(prime)))
-#    return all(int("".join(rotation)) in PRIMES for rotation in permutations(str(prime)))
-#    print([int("".join(rotation)) in PRIMES for rotation in permutations(str(prime))])
-#    rotations = [int("".join(rotation)) for rotation in permutations(str(prime))]
-#    print(rotations)
-
-    # FIXME: ROTATE not PERMUTE
-    # for rotation in permutations(str(prime)):
-    #     rotation = "".join(rotation)
-    #     print(rotation)
-    #     if len(rotation) != len(str(prime)):
-    #         return False
-    #     if not int(rotation) in PRIMES:
-    #         return False
-    # return True
-
-#        print(int("".join(rotation)))
-
+    # or: return all(rotation in PRIMES for rotation in rotations(prime)))
     for rotation in rotations(prime):
-#        print(rotation)
         if not rotation in PRIMES:
             return False
     return True
 
-
-
 def main():
-#    print(list(rotations(197)))
     circular_primes = [prime for prime in PRIMES if is_circular(prime)]
-#    print(circular_primes)
     answer = len(circular_primes)
     print(answer)
 
