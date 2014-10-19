6a64845c50cdc83f155fb4d4daca140e077390f9
Paul Foster <paulfstr@gmail.com>
2014-10-20 02:20:54 +1100

    Add outline of derivation to problem 026

--- 026_reciprocal_cycles.py
+++ 026_reciprocal_cycles.py
@@ -1,39 +1,31 @@
+# Thoughts:
+# x = 0.(123)
+# 1000x = 123.(123)
+# 999x = 123
+# x = 123/999
+#
+# x = 0.(142857)
+# 1000000x = 142857.(142857)
+# 999999x = 142857
+# x = 142857/999999
+#
+# So 1/7 = something / 999999
+# So 999999 mod 7 == 0 and the length of the cycle is the number of digits in 999999. Ie 6.
+# We'll call this length "order"
+# order is such that (10**order - 1) % n == 0
 import itertools
-    # x = 0.(123)
-    # 1000x = 123.(123)
-    # 999x = 123
-    # x = 123/999
-
-    # x = 0.(142857)
-    # 1000000x = 142857.(142857)
-    # 999999x = 142857
-    # x = 142857/999999
-
-    # find n for the biggest y such that n % (10**y - 1) == 0
-    # Here, y is known as the order of n
-# NO!!!
-# it is:
-# order such that (10**order - 1) % n == 0
 
 def recurring_length(n):
     # Find exponent such that (10**exponent - 1) % n == 0
-
     for order in range(1, n):
-        # Can optimize this exponentiation by keeping track of old exponents
-#        print(n, order, (10**order - 1 ) % 7)
+        # Could optimize this exponentiation by keeping track of old exponents
         if (10**order - 1) % n == 0:
-#            print("@@", order)
             return order
     return 0
-    # This is a bad way of doing it - the ones that don't recur will take the longest
 
 def main():
-#    answer = max((d for d in range(1, 10, 2)), key=lambda x: recurring_length(x))
-    answer = max((d for d in range(7, 1000, 2)), key=lambda x: recurring_length(x))
-    # Only odd numbers can be recurring
-#    for d in range(7, 1000, 2):
-#        pass
-#    answer = recurring_length(7)
+    answer = max((d for d in range(7, 1000, 2)),
+                 key=lambda x: recurring_length(x))
     print(answer)
 
 if __name__ == '__main__':
