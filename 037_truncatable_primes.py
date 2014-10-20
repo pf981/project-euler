6890d3a788a8d419ef2e15e27d62ea69a95c6086
Paul Foster <paulfstr@gmail.com>
2014-10-20 23:44:22 +1100

    Complete problem 037

---
+++ 037_truncatable_primes.py
@@ -0,0 +1,33 @@
+from helpers import helpers
+
+def get_truncations(num):
+    string = str(num)
+
+    # Left to right
+    for i in reversed(range(1, len(string))):
+        yield int(string[:i])
+
+    # Right to left
+    for i in reversed(range(1, len(string))):
+        yield int(string[-i:])
+
+def main():
+    # print(3797)
+    # print(list(get_truncations(3797)))
+    primes = helpers.get_million_primes()
+    truncatable_primes = []
+
+    # For each prime > 10
+    for p in primes[4:]:
+        if all(i in primes for i in get_truncations(p)):
+            truncatable_primes.append(p)
+            print(p)
+            if len(truncatable_primes) == 11:
+                break
+
+    print(truncatable_primes)
+    answer = sum(truncatable_primes)
+    print(answer)
+
+if __name__ == '__main__':
+    main()
