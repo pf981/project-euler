97a302015b4a12a5c31cab4622fb9c2b7887b0dc
Paul Foster <paulfstr@gmail.com>
2014-10-20 22:39:27 +1100

    Add is_triangular function to problem 042

--- 042_coded_triangle_numbers.py
+++ 042_coded_triangle_numbers.py
@@ -1,13 +1,19 @@
-def is_triangle(word):
-    return True
+import re
+
+def is_triangular(word):
+    sum_of_chars = sum(ord(ch) - ord('a') + 1 for ch in word.lower())
+    print(sum_of_chars)
 
 def get_words():
-    return []
+    with open("p042_words.txt") as in_file:
+        text = in_file.read()
+        return re.findall("\w+", text)
 
 def main():
-    answer = sum(1 for word in get_words() if is_triangular(word))
+    is_triangular("SKY")
 
-    answer = 0
+    answer =0
+    # answer = sum(1 for word in get_words() if is_triangular(word))
     print(answer)
 
 if __name__ == '__main__':
