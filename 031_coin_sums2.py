6de9d4bf4d09a880d4c05c4ca375484958e4861f
Paul Foster <paulfstr@gmail.com>
2014-10-20 15:54:25 +1100

    Complete problem 031

--- 031_coin_sums2.py
+++ 031_coin_sums2.py
@@ -4,6 +4,21 @@ import prettytable
 DESIRED_TOTAL = 200
 COINS = [1, 2, 5, 10, 20, 50, 100, 200]
 
+def print_table(ways_to_form):
+    table = prettytable.PrettyTable(["Target"] + COINS)
+    for target in range(10):
+        row_data = [ways_to_form[target, coin_index] for coin_index, _ in enumerate(COINS)]
+        table.add_row([target] + row_data)
+
+    table.add_row(["..."]*9)
+
+    for target in range(190, 201):
+        row_data = [ways_to_form[target, coin_index] for coin_index, _ in enumerate(COINS)]
+        table.add_row([target] + row_data)
+
+    print(table)
+
+
 def main():
     # ways_to_form[target, max_coin_index] is the number of possible ways to
     # form a total of target using coins whose index is less than
@@ -12,11 +27,13 @@ def main():
     ways_to_form = collections.defaultdict(int)
 
     # There is exactly one way to form any target using only the 1p coin
-    for target in range(1, DESIRED_TOTAL + 1):
+    for target in range(DESIRED_TOTAL + 1):
         ways_to_form[target, 0] = 1
 
-    for target in range(1, DESIRED_TOTAL + 1):
+#    for target in range(1, DESIRED_TOTAL + 1):
+    for target in range(DESIRED_TOTAL + 1):
         for coin_index, coin in enumerate(COINS[1:], start=1):
+#            print("@", coin, target, "!")
             # If the coin cannot fit in the target
             if coin > target:
                 # The ways to form will be no different than the ways to form
@@ -29,23 +46,16 @@ def main():
 
                 # AND the number of ways when using this coin
                 ways_to_form[target, coin_index] += ways_to_form[target - coin, coin_index]
+ #               print("FITS", ways_to_form[target, coin_index])
+
 
     # Generate and print the table
-    table = prettytable.PrettyTable(["Target"] + COINS)
-    for target in range(1, 10):
-        row_data = [ways_to_form[target, coin_index] for coin_index, _ in enumerate(COINS)]
-        table.add_row([target] + row_data)
-    table.add_row(["..."]*11)
-    print(table)
-
+    print_table(ways_to_form)
 
     # The solution is the number of ways to form 200 using all the coins less
     # than or equal to 200
-#    answer = ways_to_form[DESIRED_TOTAL, len(COINS) - 1]
-#    answer = ways_to_form[DESIRED_TOTAL, 1]
-#    answer = ways_to_form[1, 0]
-#    print(answer)
-#        print(ways_to_form, end="\n")
+    answer = ways_to_form[DESIRED_TOTAL, len(COINS) - 1]
+    print(answer)
 
 
 if __name__ == '__main__':
