13af96e67f1c80411c88809f8d7b875f4002b078
Paul Foster <paulfstr@gmail.com>
2014-10-21 00:32:39 +1100

    Add new prime sieve function to helpers

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -94,3 +94,15 @@ def get_million_primes():
     with open("helpers/primes.txt") as in_file:
         text = in_file.read()
     return [int(p) for p in re.findall("(\d+)", text)]
+
+def primes_to(n):
+    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = np.ones(n//3 + (n%6==2), dtype=np.bool)
+    sieve[0] = False
+    for i in range(int(n**0.5)//3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[((k*k)//3)::2*k] = False
+            sieve[(k*k+4*k-2*k*(i&1))//3::2*k] = False
+    return np.r_[2,3,((3*np.nonzero(sieve)[0]+1)|1)]
