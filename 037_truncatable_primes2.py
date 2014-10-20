db8b1bdaf2fcb413c1e23c623bfea51de39dc395
Paul Foster <paulfstr@gmail.com>
2014-10-21 00:29:44 +1100

    Optimise problem 037 by 940x by simply using a set instead of a list
    The primes were stored in a list which is inefficient when using "in" operations such as p in primes. It took 18minutes 35.668seconds for a list and 1.186seconds for a set.

--- 037_truncatable_primes2.py
+++ 037_truncatable_primes2.py
@@ -1,6 +1,22 @@
+# Notes on optimisation: The algorithm I was using was good, but storing primes in a list
+# had a MASSIVE performance hit compared to storing them as a frozenset. Simply switching to
+# a set resulted in 940 times performance!!!
+#
+# And I believe I know why! This is because I frequently use (p in PRIMES) which is much faster
+# on sets!
 from helpers import helpers
 
-PRIMES = helpers.get_million_primes()
+# Timing using a frozenset
+# real    0m1.186s
+# user    0m1.041s
+# sys     0m0.147s
+PRIMES = frozenset(helpers.get_million_primes())
+
+# Timing using a list
+# real    18m35.668s
+# user    18m29.080s
+# sys     0m4.191s
+# PRIMES = helpers.get_million_primes()
 
 def is_double_truncated(prime):
     dividend = 10
@@ -14,11 +30,11 @@ def main():
     truncatable_primes = []
 
     # For each prime > 10
-    for p in PRIMES[4:]:
+    for p in PRIMES:
         if is_double_truncated(p):
             truncatable_primes.append(p)
             print(p)
-            if len(truncatable_primes) == 11:
+            if len(truncatable_primes) == 11 + 4: # +4 to not count 2,3,5&7
                 break
 
     print(truncatable_primes)
