845ce9434b11cc706b9b4eee395919bd6024ebda
Paul Foster <paulfstr@gmail.com>
2014-10-20 15:41:54 +1100

    Print the table for problem 031

--- 031_coin_sums2.py
+++ 031_coin_sums2.py
@@ -1,4 +1,5 @@
 import collections
+import prettytable
 
 DESIRED_TOTAL = 200
 COINS = [1, 2, 5, 10, 20, 50, 100, 200]
@@ -11,12 +12,13 @@ def main():
     ways_to_form = collections.defaultdict(int)
 
     # There is exactly one way to form any target using only the 1p coin
-    for target in range(DESIRED_TOTAL + 1):
+    for target in range(1, DESIRED_TOTAL + 1):
         ways_to_form[target, 0] = 1
 
-    for target in range(DESIRED_TOTAL + 1):
-        print(target , ": ", end="")
+    for target in range(1, DESIRED_TOTAL + 1):
+ #       print(target , ": ", end="")
         for coin_index, coin in enumerate(COINS[1:], start=1):
+#            print("@", coin, target, "!")
             # If the coin cannot fit in the target
             if coin > target:
                 # The ways to form will be no different than the ways to form
@@ -24,13 +26,21 @@ def main():
                 ways_to_form[target, coin_index] = ways_to_form[target, coin_index - 1]
             # If the coin can fit
             else:
+                # FIXME: What happens in coin_index is 0???
                 # It is the ways to form the target using coins less than this one
                 ways_to_form[target, coin_index] = ways_to_form[target, coin_index - 1]
 
                 # AND the number of ways when using this coin
                 ways_to_form[target, coin_index] += ways_to_form[target - coin, coin_index]
-            print(ways_to_form[target, coin_index], end=" ")
-        print()
+#            print(ways_to_form[target, coin_index], end=" ")
+#        print()
+
+    # Generate and print the table
+    table = prettytable.PrettyTable(["Target"] + COINS)
+    for target in range(1, 10):
+        row_data = [ways_to_form[target, coin_index] for coin_index, _ in enumerate(COINS)]
+        table.add_row([target] + row_data)
+    print(table)
 
 
     # The solution is the number of ways to form 200 using all the coins less
