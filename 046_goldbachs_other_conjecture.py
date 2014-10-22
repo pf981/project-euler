db823b3c2ef767958e553bacc7a0a8489d861ad9
Paul Foster <paulfstr@gmail.com>
2014-10-23 09:53:47 +1100

    Remove commented out blocks in problem 046

--- 046_goldbachs_other_conjecture.py
+++ 046_goldbachs_other_conjecture.py
@@ -7,19 +7,7 @@ ODD_COMPOSITES = frozenset(2*n + 1 for n in range(1, MAX_RANGE // 2) if not 2*n 
 
 
 def main():
-    # print(ODD_COMPOSITES)
-    # print(PRIMES)
-    # for prime in PRIMES:
-    #     # print(prime)
-    #     for square in SQUARES:
-    #         # print(prime, square, prime + 2*square)
-    #         print(prime)
-    #     print()
-
     sums = frozenset(prime + 2*square for prime in PRIMES for square in SQUARES)
-    # sums = sorted(list(prime + 2*square for prime in PRIMES for square in SQUARES))
-    # print(sums)
-
     answer = min(ODD_COMPOSITES - sums)
     print(answer)
 
