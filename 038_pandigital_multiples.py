5f881692282efb9e6619fc6be0354ca18fccff3c
Paul Foster <paulfstr@gmail.com>
2014-10-22 13:47:46 +1100

    Complete problem 038

--- 038_pandigital_multiples.py
+++ 038_pandigital_multiples.py
@@ -9,18 +9,48 @@ PERMUTATIONS = (itertools.permutations(reversed(range(1, 10))))
 # PERMUTATIONS = frozenset(itertools.permutations(reversed(range(1, 10))))
 
 def get_largest_pandigital_multiple():
-    # FIXME: This produces incorrect results as it incorrectly assumes n = 3
-    for permutation in PERMUTATIONS:
-        first = helpers.list_to_int(permutation[:3])
-        second = helpers.list_to_int(permutation[3:6])
-        third = helpers.list_to_int(permutation[6:])
+    # # This fails to find anything, so n != 4
+    # for permutation in PERMUTATIONS:
+    #     first = helpers.list_to_int(permutation[:2])
+    #     second = helpers.list_to_int(permutation[2:4])
+    #     third = helpers.list_to_int(permutation[4:6])
+    #     fourth = helpers.list_to_int(permutation[6:])
 
-        if first > second or second > third:
+    #     if first > second or second > third or third > fourth:
+    #         continue
+
+    #     print(first, second, third, fourth)
+
+    #     if second % 2 == 0 and second // 2 == first and \
+    #        third % 3 == 0 and third // 3 == first and \
+    #        fourth % 4 == 0 and fourth // 4 == first:
+    #         print(first, second, third, fourth)
+    #         return first
+
+    # This finds 9327, which is not correct, so n != 2
+    for permutation in PERMUTATIONS:
+        first = helpers.list_to_int(permutation[:4])
+        second = helpers.list_to_int(permutation[4:])
+
+        if first > second:
             continue
 
-        if second % 2 == 0 and third % 3 == 0 and second // 2 == first and third // 3 == first:
-            print(first, second, third)
+        if second % 2 == 0 and second // 2 == first:
+            print(first, second)
             return first
+
+    # # This find 327 which is not correct, so n != 3
+    # for permutation in PERMUTATIONS:
+    #     first = helpers.list_to_int(permutation[:3])
+    #     second = helpers.list_to_int(permutation[3:6])
+    #     third = helpers.list_to_int(permutation[6:])
+
+    #     if first > second or second > third:
+    #         continue
+
+    #     if second % 2 == 0 and third % 3 == 0 and second // 2 == first and third // 3 == first:
+    #         print(first, second, third)
+    #         return first
 
 # def get_largest_pandigital_multiple():
 #     for num in reversed(range(1, PANDIGITAL_MULTIPLE_UPPER_BOUND + 1)):
