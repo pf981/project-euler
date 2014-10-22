6f1a06ff80c93db43a3b16c9aeae940886f6c897
Paul Foster <paulfstr@gmail.com>
2014-10-22 13:53:18 +1100

    Tidy code for problem 038

--- 038_pandigital_multiples.py
+++ 038_pandigital_multiples.py
@@ -1,72 +1,21 @@
-# The largest number that it could possibly be is x such that x + 2x = 987654321.
-# This implies the largest pandigital is less than or equal to 329,218,107
+# This solution assumes n=2. If this solution failed, I would rewrite it with
+# n=3 and n=4, but this solution is sufficient.
 import itertools
 from helpers import helpers
 
-PANDIGITAL_MULTIPLE_UPPER_BOUND = 329218107
-#PERMUTATIONS = (helpers.list_to_int(x) for x in itertools.permutations(reversed(range(1, 10))))
 PERMUTATIONS = (itertools.permutations(reversed(range(1, 10))))
-# PERMUTATIONS = frozenset(itertools.permutations(reversed(range(1, 10))))
 
 def get_largest_pandigital_multiple():
-    # # This fails to find anything, so n != 4
-    # for permutation in PERMUTATIONS:
-    #     first = helpers.list_to_int(permutation[:2])
-    #     second = helpers.list_to_int(permutation[2:4])
-    #     third = helpers.list_to_int(permutation[4:6])
-    #     fourth = helpers.list_to_int(permutation[6:])
-
-    #     if first > second or second > third or third > fourth:
-    #         continue
-
-    #     print(first, second, third, fourth)
-
-    #     if second % 2 == 0 and second // 2 == first and \
-    #        third % 3 == 0 and third // 3 == first and \
-    #        fourth % 4 == 0 and fourth // 4 == first:
-    #         print(first, second, third, fourth)
-    #         return first
-
-    # This finds 9327, which is not correct, so n != 2
+    # For each permutation
     for permutation in PERMUTATIONS:
+        # Split the permutation in two. Note that the only split that makes
+        # sense is the the first being 4 digits and the second being 5
+        # digits. This is because second = 2*first.
         first = helpers.list_to_int(permutation[:4])
         second = helpers.list_to_int(permutation[4:])
 
-        if first > second:
-            continue
-
-        if second % 2 == 0 and second // 2 == first:
-            print(first, second)
-            return first
-
-    # # This find 327 which is not correct, so n != 3
-    # for permutation in PERMUTATIONS:
-    #     first = helpers.list_to_int(permutation[:3])
-    #     second = helpers.list_to_int(permutation[3:6])
-    #     third = helpers.list_to_int(permutation[6:])
-
-    #     if first > second or second > third:
-    #         continue
-
-    #     if second % 2 == 0 and third % 3 == 0 and second // 2 == first and third // 3 == first:
-    #         print(first, second, third)
-    #         return first
-
-# def get_largest_pandigital_multiple():
-#     for num in reversed(range(1, PANDIGITAL_MULTIPLE_UPPER_BOUND + 1)):
-#         digits = []
-#         # print(num)
-#         for multiplier in itertools.count(1):
-#             digits.extend(list(helpers.int_to_digits(num * multiplier)))
-#             # print(num, "*", multiplier, "=", num*multiplier)
-#             # print(digits)
-
-#             if len(digits) > 9:
-#                 break
-#             if len(digits) < 9 or multiplier == 1:
-#                 continue
-#             if tuple(digits) in PERMUTATIONS:
-#                 return num
+        if second == 2 * first:
+            return int(str(first) + str(second))
 
 def main():
    answer = get_largest_pandigital_multiple()
