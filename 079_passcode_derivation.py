e8df04a5ec54305387538a5363310e6d9088a481
Paul Foster <paulfstr@gmail.com>
2014-10-26 23:46:48 +1100

    Add graph generation to problem 079

--- 079_passcode_derivation.py
+++ 079_passcode_derivation.py
@@ -1,4 +1,24 @@
+import collections
 import re
+
+def connections(attempt):
+    for i, digit1 in enumerate(attempt):
+        for digit2 in attempt[i+1:]:
+            yield digit1, digit2
+
+def make_graph(all_attempts):
+    """
+    Returns a dictionary where the value is a list of the digits that must follow the key
+    e.g. attempt "319" means graph['3'] = ['1', '9'] and graph['1'] = ['9']
+    """
+    graph = collections.defaultdict(set)
+
+    for attempt in all_attempts:
+        for before, after in connections(attempt):
+            # "after" appears after "before"
+            graph[before].add(after)
+
+    return graph
 
 def main():
     with open("p079_keylog.txt") as in_file:
@@ -6,9 +26,12 @@ def main():
 
     all_attempts = re.findall("(\d\d\d)", text)
 
+    graph = make_graph(all_attempts)
+    print(graph)
+
     # possible_numbers = set(digit for digit in attempt for attempt in all_attempts)
-    possible_numbers = set(digit for attempt in all_attempts for digit in attempt)
-    print(possible_numbers)
+    # possible_numbers = set(digit for attempt in all_attempts for digit in attempt)
+    # print(possible_numbers)
     # passcode_guess = ''
 
     # # for number in (triple for triple in numbers):
