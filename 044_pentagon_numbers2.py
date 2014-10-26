ad02c9ed0c04798fdf4c09b4da339ab7a8ea7faa
Paul Foster <paulfstr@gmail.com>
2014-10-26 22:51:54 +1100

    Add a more efficient solution to problem 044

---
+++ 044_pentagon_numbers2.py
@@ -0,0 +1,27 @@
+import itertools
+
+def isPenthagonal(num):
+    n = ((num * 24 + 1)**0.5 + 1) / 6
+    return n == int(n)
+
+def get_min_distance():
+    # pentagonals is the set of pentagonals that were previously generated
+    pentagonals = set()
+
+    # For every pentagonal
+    for pentagonal in (n * (3 * n - 1) // 2 for n in itertools.count(1)):
+        for pentagonal2 in pentagonals:
+            # If the sum and difference are pentagonal
+            if pentagonal - pentagonal2 in pentagonals and isPenthagonal(pentagonal + pentagonal2):
+                # Return the distance
+                return pentagonal - pentagonal2
+
+        # Update the list of pentagonals
+        pentagonals.add(pentagonal)
+
+def main():
+    answer = get_min_distance()
+    print(answer)
+
+if __name__ == '__main__':
+    main()
