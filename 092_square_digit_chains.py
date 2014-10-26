1444834b50987685d9fc42dd5e7c368faf70dcea
Paul Foster <paulfstr@gmail.com>
2014-10-27 01:33:24 +1100

    Add comments to problem 092

--- 092_square_digit_chains.py
+++ 092_square_digit_chains.py
@@ -1,6 +1,9 @@
 MAX_RANGE = 10000000
 
 def generate_digit_squares(n):
+    """
+    Generates an infinite chain of sum of squares of the digits
+    """
     chain_element = n
     while True:
         yield chain_element
@@ -21,21 +24,22 @@ def main():
         # then we know every element leads to that same number
         current_chain = set()
 
+        # For each element in this chain
         for chain_element in generate_digit_squares(n):
+            # If it is an element known to lead to 1
             if chain_element in numbers_arriving_at_1:
+                # Then every element of this chain must lead to 1
                 numbers_arriving_at_1 |= current_chain
                 break
 
+            # If it is an element known to lead to 89
             if chain_element in numbers_arriving_at_89:
+                # Then every element of this chain must lead to 89
                 numbers_arriving_at_89 |= current_chain
                 break
 
             current_chain.add(chain_element)
 
-
-
-    # print(89, numbers_arriving_at_89)
-    # print(1, numbers_arriving_at_1)
     answer = len(numbers_arriving_at_89)
     print(answer)
 
