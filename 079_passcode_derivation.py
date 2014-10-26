ed92d48168939fe219c2535d1f4999a02e7d1d78
Paul Foster <paulfstr@gmail.com>
2014-10-27 00:33:43 +1100

    Removed original attempt at problem 079

--- 079_passcode_derivation.py
+++ 079_passcode_derivation.py
@@ -1,13 +1,33 @@
 import collections
+import itertools
 import re
 
-def find_smallest_password(vertex, graph):
-    pass
+def is_possible_password(candidate, graph):
+    """
+    Returns true if the candidate would satisfy the password requirements
+    """
+    # For each digit that has known digits that must follow it
+    for key in graph:
+        index_of_key = candidate.find(key)
+
+        # If the key is not present in the candidate password
+        if index_of_key == -1:
+            return False
+
+        # For each digit that must follow the key
+        for must_follow_key in graph[key]:
+            # If that digit cannot be found after the key
+            if candidate.find(must_follow_key, index_of_key) == -1:
+                return False
+    return True
 
 def connections(attempt):
+    # digit1 is the key
     for i, digit1 in enumerate(attempt):
+        # digit2 is the value. The key must be followed by the value in the password
         for digit2 in attempt[i+1:]:
             yield digit1, digit2
+
 
 def make_graph(all_attempts):
     """
@@ -29,43 +49,22 @@ def main():
 
     all_attempts = re.findall("(\d\d\d)", text)
 
+    # Assume that each of the digits only appears once. If this fails, then the assumption is wrong.
+    possible_digits = set(digit for attempt in all_attempts for digit in attempt)
+
+    # graph is a dictionary whose values are the digits that must follow the key
     graph = make_graph(all_attempts)
-    print(graph)
 
-    possible_passwords = []
+    # Try all permutations of possible digits
+    for permutation in itertools.permutations(possible_digits):
+        candidate_password = ''.join(permutation)
 
-    # For each starting point
-    for starting_vertex in graph:
-        # print(vertex)
-        password = find_smallest_password(starting_vertex, graph)
+        # If this permutation is legitimate
+        if (is_possible_password(candidate_password, graph)):
+            answer = ''.join(candidate_password)
+            break
 
-        if password:
-            possible_passwords.append(password)
-
-    # possible_numbers = set(digit for digit in attempt for attempt in all_attempts)
-    # possible_numbers = set(digit for attempt in all_attempts for digit in attempt)
-    # print(possible_numbers)
-    # passcode_guess = ''
-
-    # # for number in (triple for triple in numbers):
-    # for triple in numbers:
-    #     index = 0
-    #     for digit in triple:
-    #         print(digit)
-
-    #         index = passcode_guess.find(digit, index)
-    #         if index == -1:
-    #             passcode_guess += digit
-    #     # print(index)
-    #     # break
-
-    # print(passcode_guess)
-
-    # for possible_passcode in itertools.count(1):
-    #     if is
-
-    # answer = len(passcode_guess) + 1
-    # print(answer)
+    print(answer)
 
 if __name__ == '__main__':
     main()
