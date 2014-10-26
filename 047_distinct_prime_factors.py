54daa52d3e46c32736d5c47cfcac99c19a2eb472
Paul Foster <paulfstr@gmail.com>
2014-10-26 18:09:57 +1100

    Refactor problem 047 to remove for-else

--- 047_distinct_prime_factors.py
+++ 047_distinct_prime_factors.py
@@ -3,18 +3,21 @@ from helpers import helpers
 
 NUM_CONSECUTIVE = 2
 
+def satisfies_criteria(candidate):
+    for num_consecutive in range(NUM_CONSECUTIVE):
+        cur_factors = set(helpers.prime_factors(candidate + num_consecutive))
+
+        # If there aren't the right number of factors
+        if len(cur_factors) != NUM_CONSECUTIVE:
+            return False
+    return True
+
+
 def main():
-    for i in itertools.count(4):
-        for num_consecutive in range(NUM_CONSECUTIVE):
-            cur_factors = set(helpers.prime_factors(i + num_consecutive))
-
-            # If there aren't the right number of factors
-            if len(cur_factors) != NUM_CONSECUTIVE:
-                break
-        else:
-            answer = i
+    for candidate in itertools.count(4):
+        if satisfies_criteria(candidate):
+            answer = candidate
             break
-
 
     print(answer)
 
