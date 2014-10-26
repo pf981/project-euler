62a0fea16a37d7c79f1f350b31e631252d851709
Paul Foster <paulfstr@gmail.com>
2014-10-26 17:52:41 +1100

    Tidy code in problem 043

--- 043_sub-string_divisibility.py
+++ 043_sub-string_divisibility.py
@@ -1,45 +1,20 @@
-import collections
 import itertools
-import sys
-from helpers import helpers
-
-def is_pandigital(digits_string):
-    #digits = list(helpers.int_to_digits(n))
-    # print(len(set(digits)))
-    # print("@@@")
-    return len(digits_string) == len(set(digits_string))
-
-# def is_pandigital(n):
-#     digits = list(helpers.int_to_digits(n))
-#     # print(len(set(digits)))
-#     # print("@@@")
-#     return len(digits) == len(set(digits))
 
 def is_sub_string_divisible(digits_str):
+    """
+    Returns True if the substrings are divisible by primes
+    """
     for i, prime in enumerate([2, 3, 5, 7, 11, 13 ,17], start=1):
-        # If the substring doesn't satisfy the disivisibility criteria
         if int(digits_str[i:i+3]) % prime != 0:
-            # print(digits_str[i:i+3], prime)
             return False
     return True
 
-
 def main():
-    # print(is_pandigital("12345"))
-    # print(is_pandigital("123145"))
-    # print(is_sub_string_divisible("1406357289"))
-    # print(is_sub_string_divisible("1406357829"))
+    # Find the sum of all sub-string divisible 9-digit pandigitals
     answer = sum(int(''.join(s))
                  for s in itertools.permutations('1234567890')
                  if is_sub_string_divisible(''.join(s)))
-    # special_pandigitals = [int(''.join(s)) for s in itertools.permutations('1234567890')]
-    # answer = 0
-    # print(special_pandigitals)
-    # answer = 0
-    # for s in itertools.permutations('1234567890'):
-    #     print(int(''.join(s)))
     print(answer)
-    return 0
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
