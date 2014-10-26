c742720767fb3f523fb82101644b1bc5ae1374a0
Paul Foster <paulfstr@gmail.com>
2014-10-26 22:54:03 +1100

    Add comments in problem 044 justifying why we can check if the difference is in the set but not the sum

--- 044_pentagon_numbers2.py
+++ 044_pentagon_numbers2.py
@@ -12,6 +12,8 @@ def get_min_distance():
     for pentagonal in (n * (3 * n - 1) // 2 for n in itertools.count(1)):
         for pentagonal2 in pentagonals:
             # If the sum and difference are pentagonal
+            # Note that we would have generated the difference, but not the
+            # sum as it would be bigger than the current pentagonal
             if pentagonal - pentagonal2 in pentagonals and isPenthagonal(pentagonal + pentagonal2):
                 # Return the distance
                 return pentagonal - pentagonal2
