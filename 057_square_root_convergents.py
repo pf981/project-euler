e3d573192e67e53b548e0dcd63957ae97925003f
Paul Foster <paulfstr@gmail.com>
2014-10-27 16:16:42 +1100

    Complete problem 057

--- 057_square_root_convergents.py
+++ 057_square_root_convergents.py
@@ -9,21 +9,54 @@ from fractions import Fraction
 # def sqrt2_approximation(terms):
 #     return 1 + 1 /(denom(terms))
 
-def sqrt2_approximation(terms):
-    # denominator[1] == 1.5 means the first denominator is 1.5
+def compute_denominators(max_terms):
     denominator = {}
-    denominator[1] = 2
+    denominator[1] = Fraction(2)
 
-    for i in range(2, terms + 1):
+    for i in range(2, max_terms + 1):
         denominator[i] = 2 + 1/denominator[i-1]
 
-    print(denominator)
+    return denominator
 
-    return 1 + 1/denominator[terms]
+# def compute_denominators(max_terms):
+#     denominator = {}
+#     denominator[1] = 2
 
+#     for i in range(2, max_terms + 1):
+#         denominator[i] = 2 + 1/denominator[i-1]
+
+#     return denominator
+
+# def sqrt2_approximation(terms):
+#     # denominator[1] == 1.5 means the first denominator is 1.5
+#     denominator = {}
+#     denominator[1] = 2
+
+#     for i in range(2, terms + 1):
+#         denominator[i] = 2 + 1/denominator[i-1]
+
+#     print(denominator)
+
+#     return 1 + 1/denominator[terms]
+
+
+def compute_nth_term(n, denominator):
+    return Fraction(1, 1) + Fraction(1, denominator[n])
 
 def main():
-    print(sqrt2_approximation(4))
+    denominator = compute_denominators(1000)
+    terms = []
+
+    for i in range(1, 1001):
+        # terms.append(Fraction(1 + 1/denominator[i]))
+        terms.append(compute_nth_term(i, denominator))
+
+    answer = sum(1
+                 for fract in terms
+                 if len(str(fract.numerator)) > len(str(fract.denominator)))
+
+
+    # print(sqrt2_approximation(4))
     # print(sqrt2_approximation(0))
     # print(sqrt2_approximation(1))
     # print(sqrt2_approximation(2))
@@ -37,7 +70,6 @@ def main():
 
 
 
-    answer = 0
     print(answer)
 
 
