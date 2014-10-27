e5493c9f9322633decab2a946f17983d80a4f652
Paul Foster <paulfstr@gmail.com>
2014-10-27 16:21:25 +1100

    Tidy code and add comments to problem 057

--- 057_square_root_convergents.py
+++ 057_square_root_convergents.py
@@ -1,15 +1,11 @@
 from fractions import Fraction
 
-# def denom(terms):
-#     if terms == 0:
-#         return 2
-
-#     return 2 + 1/denom(terms-1)
-
-# def sqrt2_approximation(terms):
-#     return 1 + 1 /(denom(terms))
-
 def compute_denominators(max_terms):
+    """
+    Returns a list of Fractions which represent the denominators in the sequence.
+    For example, denominator[1] = 2, so the first iteration is 1 + 1/2
+    denominator[2] = 2.5, so the second iteration is 1 + 1/2.5
+    """
     denominator = {}
     denominator[1] = Fraction(2)
 
@@ -18,57 +14,25 @@ def compute_denominators(max_terms):
 
     return denominator
 
-# def compute_denominators(max_terms):
-#     denominator = {}
-#     denominator[1] = 2
-
-#     for i in range(2, max_terms + 1):
-#         denominator[i] = 2 + 1/denominator[i-1]
-
-#     return denominator
-
-# def sqrt2_approximation(terms):
-#     # denominator[1] == 1.5 means the first denominator is 1.5
-#     denominator = {}
-#     denominator[1] = 2
-
-#     for i in range(2, terms + 1):
-#         denominator[i] = 2 + 1/denominator[i-1]
-
-#     print(denominator)
-
-#     return 1 + 1/denominator[terms]
-
-
 def compute_nth_term(n, denominator):
-    return Fraction(1, 1) + Fraction(1, denominator[n])
+    """
+    Returns the nth iteration of the sequence
+    """
+    return 1 + Fraction(1, denominator[n])
 
 def main():
     denominator = compute_denominators(1000)
     terms = []
 
+    # For each term from 1 to 1000
     for i in range(1, 1001):
-        # terms.append(Fraction(1 + 1/denominator[i]))
+        # Compute the value of the term and stor it in the terms list
         terms.append(compute_nth_term(i, denominator))
 
+    # The answer is the number terms whose numerator is larger than the denominator
     answer = sum(1
                  for fract in terms
                  if len(str(fract.numerator)) > len(str(fract.denominator)))
-
-
-    # print(sqrt2_approximation(4))
-    # print(sqrt2_approximation(0))
-    # print(sqrt2_approximation(1))
-    # print(sqrt2_approximation(2))
-    # print(sqrt2_approximation(3))
-    # print(sqrt2_approximation(4))
-    # result = 1
-    # for _ in range(100):
-    #     result
-    # fractions.append(cur + 1/2)
-    # cur += 1/(2 + 1/2)
-
-
 
     print(answer)
 
