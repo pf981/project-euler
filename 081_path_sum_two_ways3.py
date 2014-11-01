c330ab81e3019cf491f7fbd9608a8e18f736c43d
Paul Foster <paulfstr@gmail.com>
2014-11-02 01:25:46 +1100

    In problem 081, rename tree variable to matrix as it is not a tree.

--- 081_path_sum_two_ways3.py
+++ 081_path_sum_two_ways3.py
@@ -1,20 +1,20 @@
 import numpy as np
 
-def iterate_zigzag(tree):
+def iterate_zigzag(matrix):
     """
-    Iterates over the tree in the following order
+    Iterates over the matrix in the following order
     17 15 13 10
     14 12 9  6
     11 8  5  3
     7  4  2  1
     """
     # In the docstring example, this iterates through 1 to 10
-    for start_column in reversed(range(len(tree))):
-        for row in reversed(range(start_column + 1, len(tree))):
-            yield (row, len(tree) + start_column - row)
+    for start_column in reversed(range(len(matrix))):
+        for row in reversed(range(start_column + 1, len(matrix))):
+            yield (row, len(matrix) + start_column - row)
 
     # In the docstring example, this iterates through 11 to 17
-    for start_row in reversed(range(len(tree))):
+    for start_row in reversed(range(len(matrix))):
         for column in range(start_row + 1):
             yield (start_row - column, column)
 
@@ -25,20 +25,20 @@ def min_of_not_none(a, b):
     return min(element for element in [a, b] if element != None)
 
 
-def length_of_shortest_path(tree):
+def length_of_shortest_path(matrix):
     """
-    Returns the length of the shortest path through a tree where you can only
+    Returns the length of the shortest path through a matrix where you can only
     traverse down or right
     """
     # Use a dynamic programing approach. Start from the bottom right and
     # update each element so that they equal their value plus the minimum of
     # the element to the right or down
-    for x, y in iterate_zigzag(tree):
-        right = tree[x+1][y] if x+1 < len(tree) else None
-        down = tree[x][y+1] if y+1 < len(tree) else None
-        tree[x][y] += min_of_not_none(right, down)
+    for x, y in iterate_zigzag(matrix):
+        right = matrix[x+1][y] if x+1 < len(matrix) else None
+        down = matrix[x][y+1] if y+1 < len(matrix) else None
+        matrix[x][y] += min_of_not_none(right, down)
 
-    return tree[0][0]
+    return matrix[0][0]
 
 
 def main():
