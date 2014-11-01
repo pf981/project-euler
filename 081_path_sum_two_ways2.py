b9a314693c8866d1e3229dbf77c1ddff660ddf53
Paul Foster <paulfstr@gmail.com>
2014-11-02 00:52:54 +1100

    Reverse zigzag iteration in problem 81.

--- 081_path_sum_two_ways2.py
+++ 081_path_sum_two_ways2.py
@@ -7,6 +7,29 @@ MATRIX = np.array([[131, 673, 234, 103, 18],
                    [537, 699, 497, 121, 956],
                    [805, 732, 524, 37, 331]])
 
+# def iterate_zigzag(tree):
+#     """
+#     Iterates over the tree in the following order
+#     1  3  6  9
+#     2  5  8  11
+#     4  7  10 12
+#     """
+#     # print(tree[0][0])
+#     # print()
+#     for start_row in range(len(tree)):
+#         for column in range(start_row + 1):
+#             # print(start_row, column, tree[start_row - column][column])
+#             yield (start_row - column, column)
+#     # print()
+#     for start_column in range(len(tree)):
+#         for row in reversed(range(start_column + 1, len(tree))):
+#             # print(start_column, row, tree[row][start_column - row])
+#             yield (row, start_column - row)
+
+
+#     # return [(1, 2), (1, 2)]
+
+
 def iterate_zigzag(tree):
     """
     Iterates over the tree in the following order
@@ -14,18 +37,25 @@ def iterate_zigzag(tree):
     2  5  8  11
     4  7  10 12
     """
-    print(tree[0][0])
-    print()
-    for start_row in range(len(tree)):
-        for column in range(start_row + 1):
-            print(start_row, column, tree[start_row - column][column])
-    print()
-    for start_column in range(len(tree)):
+    # print(tree[0][0])
+    for start_column in reversed(range(len(tree))):
         for row in reversed(range(start_column + 1, len(tree))):
             print(start_column, row, tree[row][start_column - row])
+            yield (row, start_column - row)
+
+    print()
+    print()
+    for start_row in reversed(range(len(tree))):
+        for column in range(start_row + 1):
+            print(start_row, column, tree[start_row - column][column])
+            yield (start_row - column, column)
+    print()
+    print()
+    print()
 
 
-    return [(1, 2), (1, 2)]
+    # return [(1, 2), (1, 2)]
+
 
 def min_of_not_none(a, b):
     return min(element for element in [a, b] if element != None)
@@ -37,9 +67,14 @@ def length_of_shortest_path(tree):
     traverse down or right
     """
     for x, y in iterate_zigzag(tree):
-        tree[x][y] += min_of_not_none(
-            tree[x+1][y], # Right
-            tree[x][y+1]) # Down
+        right = tree[x+1][y] if x+1 < len(tree) else None
+        down = tree[x][y+1] if y+1 < len(tree) else None
+        # tree[x][y] += min_of_not_none(
+        #     tree[x+1][y], # Right
+        #     tree[x][y+1]) # Down
+        tree[x][y] += min_of_not_none(right, down)
+
+    print(tree)
 
     return tree[0][0]
 
