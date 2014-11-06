301c3070a35b9d11c3ce0c8d2c46b52c691fba6a
Paul Foster <paulfstr@gmail.com>
2014-11-06 19:00:21 +1100

    Fix problem 062 where permutations were not unique

--- 062_cubic_permutations.py
+++ 062_cubic_permutations.py
@@ -1,22 +1,31 @@
 import itertools
 
+def is_cube(n):
+    return round(n**(1/3.0))**3 == n
+
 def main():
     # for n in itertools.count(1):
     for n in range(345, 346):
-        cube = sorted(str(n**3))
-        print(cube)
-        for permutation in itertools.permutations(str(n)):
-            print(''.join(permutation), (int(''.join(permutation))**3))
-            if cube == sorted(str(int(''.join(permutation))**3)):
-                print(int(''.join(permutation)))
+        cube = n**3
         counts = sum(1
-                     for permutation in itertools.permutations(str(n))
-                     if cube == sorted(str(int(''.join(permutation))**3)))
+                     for permutation in set(itertools.permutations(str(cube)))
+                     if is_cube(int(''.join(permutation))))
+
+        # for s in set(''.join(l) for l in list(itertools.permutations(str(cube)))):
+        #     print("@",s)
+
+        for permutation in set(itertools.permutations(str(cube))):
+            if is_cube(int(''.join(permutation))):
+                print(''.join(permutation))
+
         print(counts)
         if counts == 3:
             answer = n # FIXME: This is not the answer
             break
-    print(answer)
+    # print(answer)
 
 if __name__ == '__main__':
     main()
+
+
+# Alternative: Generate all cubes up to a point, sort digits and count them. Find the one with 5 permutations
