7239adb8c2e3f1663a13d7faade429a0be43a464
Paul Foster <paulfstr@gmail.com>
2014-11-08 02:29:12 +1100

    Tidy code in problem 112

--- 112_bouncy_numbers.py
+++ 112_bouncy_numbers.py
@@ -3,6 +3,10 @@ import itertools
 TARGET_PROPORTION = 0.99
 
 def is_bouncy(n):
+    """
+    Returns true if n's digits are not in ascending or descending order
+    False otherwise
+    """
     list_n = list(str(n))
     left_to_right = sorted(list_n)
     right_to_left = list(reversed(left_to_right))
@@ -15,6 +19,8 @@ def main():
     for n in itertools.count(1):
         if is_bouncy(n):
             num_bouncy += 1
+
+        # Calculate the proportion of bouncy to non-bouncy numbers
         if num_bouncy / n > TARGET_PROPORTION:
             answer = n - 1
             break
