99a3eff9b96443b01eddf631bfa29b51bf59566a
Paul Foster <paulfstr@gmail.com>
2014-11-08 03:47:25 +1100

    Compute pairs of primes which can concatenate to become another prime in problem 060

--- 060_prime_pair_sets.py
+++ 060_prime_pair_sets.py
@@ -1,3 +1,4 @@
+import collections
 import sympy
 from sympy.ntheory.primetest import isprime
 
@@ -8,19 +9,43 @@ def concat_ints(a, b):
     return int(str(a) + str(b))
 
 def is_cat_pair(pair):
-    return isprime(concat_ints(pair[0], pair[1])) and isprime(concat_ints(pair[0], pair[1]))
+    return isprime(concat_ints(pair[0], pair[1]))
+    # return isprime(concat_ints(pair[0], pair[1])) and isprime(concat_ints(pair[0], pair[1]))
 
 def main():
     primes = list(sympy.sieve.primerange(2, MAX_PRIMES))
-    print(is_cat_pair((109, 673)))
-    print(is_cat_pair((109, 674)))
 
-    pairs = [(p1, p2)
-             for i, p1 in enumerate(primes)
-             for p2 in primes[i+1:]]
+    # all_pairs is a list of tuples of every combination of primes
+    # FIXME: Maybe do all combinations, but only check the left concatenation
+    all_pairs = [(p1, p2)
+             for p1 in primes
+             for p2 in primes]
+    # all_pairs = [(p1, p2)
+    #          for i, p1 in enumerate(primes)
+    #          for p2 in primes[i+1:]]
+
+    # special_pairs is a subset of all_pairs which only contains elements
+    # whose pair can be concatenated to form a prime
+    # paired_with = {p1: p2 for p1, p2 in all_pairs if is_cat_pair((p1, p2))}
+    paired_with = collections.defaultdict(list)
+    for p1, p2 in all_pairs:
+        if is_cat_pair((p1, p2)):
+            paired_with[p1].append(p2)
+
+    print(paired_with)
+
+
+    # special_pairs = [pair for pair in all_pairs if is_cat_pair(pair)]
+
+    # for starter in primes:
+    #     final_primes = [starter].extend(paired_with[starter])
+    #     print(started, final_primes)
+
+
+    # print(special_pairs)
 
     print(primes)
-    print(pairs)
+    # print(pairs)
 
     pass
 
