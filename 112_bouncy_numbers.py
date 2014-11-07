448133ec8f88bca0b519fe507f15e4ab9e7aaaf1
Paul Foster <paulfstr@gmail.com>
2014-11-08 02:27:38 +1100

    Complete problem 112

--- 112_bouncy_numbers.py
+++ 112_bouncy_numbers.py
@@ -1,13 +1,13 @@
 import itertools
 
-TARGET_PROPORTION = 0.5
+TARGET_PROPORTION = 0.99
 
 def is_bouncy(n):
     list_n = list(str(n))
-    left_bouncy = sorted(list_n)
-    right_bouncy = list(reversed(left_bouncy))
+    left_to_right = sorted(list_n)
+    right_to_left = list(reversed(left_to_right))
 
-    return list_n == left_bouncy or list_n == right_bouncy
+    return list_n != left_to_right and list_n != right_to_left
 
 def main():
     num_bouncy = 0
@@ -16,7 +16,7 @@ def main():
         if is_bouncy(n):
             num_bouncy += 1
         if num_bouncy / n > TARGET_PROPORTION:
-            answer = n
+            answer = n - 1
             break
 
     print(answer)
