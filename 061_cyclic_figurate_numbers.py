1ebbe73bb65caae117d69b284a48aa238954d80a
Paul Foster <paulfstr@gmail.com>
2014-11-08 06:29:40 +1100

    Add generate_valid_paths function to problem 061

--- 061_cyclic_figurate_numbers.py
+++ 061_cyclic_figurate_numbers.py
@@ -1,11 +1,34 @@
 #import itertools
 import collections
 
-# def generate_cycle(n):
-#     n_deque = collections.deque(str(n))
-# #    for _ in len(n):
-#     print(n_deque)
+# How many cyclic numbers we are trying to find
+TARGET_NUMBERS = 3
 
+def generate_valid_paths(tree):
+    """
+    This generates paths from a depth-first tree traversal such that the path
+    is TARGET_PAIRS long and every element is adjacent to every other element
+    """
+    for node, _ in tree.items():
+        # nodes_to_visit is a list of tuples. The first element of the tuple
+        # is the node to visit. The second is a list representing the path
+        # taken to get to that node.
+        nodes_to_visit = [(node, set())]
+
+        while nodes_to_visit:
+            cur_node = nodes_to_visit.pop()
+            cur_path = cur_node[1] | {cur_node[0]}
+
+            for child in tree[cur_node[0]]:
+                # If the child hasn't been visited and the child contains
+                # every element in the path
+                if child not in cur_path:
+                    # Prepend the node and the path
+                    nodes_to_visit.insert(0, (child, cur_path))
+
+            if len(cur_path) == TARGET_NUMBERS:
+                yield cur_path
+# FIXME: USE LIST
 
 def main():
     # Maybe map every two digit number to pentagonal numbers that start with those digits
@@ -26,7 +49,10 @@ def main():
         if num in polygonals:
             cycle_map[str(num)[:2]].add(str(num)[2:])
 
-    print(cycle_map)
+    for path in generate_valid_paths(cycle_map):
+        print(path)
+
+    # print(cycle_map)
         # # If the second last digit is zero, then at least one of the cycles will not be
         # # four digits
         # if str(num)[-2]:
