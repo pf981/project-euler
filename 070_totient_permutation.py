a196ef7d94eef4732dd7c599f579ecb5ecf2c80d
Paul Foster <paulfstr@gmail.com>
2014-11-08 21:35:41 +1100

    Complete problem 070

--- 070_totient_permutation.py
+++ 070_totient_permutation.py
@@ -1,20 +1,34 @@
-import sys
+import math
+import sympy
 
-UPPER_BOUND = 100
+UPPER_BOUND = 10**7
+# PRIMES = list(sympy.sieve.primerange(2, 1/2 * UPPER_BOUND))
+PRIMES = list(sympy.sieve.primerange(2, 1.5 * math.sqrt(UPPER_BOUND)))
+# PRIMES = list(sympy.sieve.primerange(2, math.sqrt(UPPER_BOUND)))
 
 def is_permutation(a, b):
     return sorted(str(a)) == sorted(str(b))
 
-def tot(n):
-    pass
+# Maybe keep a list of what is relatively prime to what
 
 def main():
     answer = None
     best_ratio = None
 
-    for n in range(UPPER_BOUND + 1):
-        totient = tot(n)
+    prime_pairs = ((p1, p2) for p1 in PRIMES for p2 in PRIMES)
+
+    for p1, p2 in prime_pairs:
+        # Construct n as the product of two primes
+        n = p1 * p2
+
+        if n > UPPER_BOUND:
+            continue
+
+        # As n is the product of two primes, the phi is simply (p1-1)(p2-1)
+        totient = (p1 - 1) * (p2 - 1)
+
         if is_permutation(n, totient):
+            print("@", n, totient)
             if not best_ratio or n / totient < best_ratio:
                 answer = n
                 best_ratio = n / totient
