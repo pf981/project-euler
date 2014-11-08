f6c3bd8c7559d7ec2fb63c0d837b2e47d775a001
Paul Foster <paulfstr@gmail.com>
2014-11-08 17:37:04 +1100

    Tidy code in problem 089

--- 089_roman_numerals.py
+++ 089_roman_numerals.py
@@ -1,81 +1,32 @@
 import re
-# 1000 M
-# 900 CM
-# 500 D
-# 400 CD
-# 100 C
-# 90 XC
-# 50 L
-# 40 XL
-# 10 X
-# 9 IX
-# 5 V
-# 4 IV
-# 1 I
 value = {}
 value['M'] = 1000
-value['CM'] = 900
 value['D'] = 500
-value['CD'] = 400
 value['C'] = 100
-value['XC'] = 90
 value['L'] = 50
-value['XL'] = 40
 value['X'] = 10
-value['IX'] = 9
 value['V'] = 5
-value['IV'] = 4
 value['I'] = 1
-
-# There is probably a way to simplify the given roman numaral that doesn't
-# involve finding the decimal representation
-
-# Note that this doesn't put the characters in the correct order, it just does
-# the substitutions. The correct order is not needed as we are only interested
-# in counting how many characters there are
-# def simplify_roman(original):
-#     fixed = re.sub(r'IIII', 'IV', original)
-#     fixed = re.sub(r'VIV', 'IX', fixed)
-#     fixed = re.sub(r'XXXX', 'XL', fixed)
-#     fixed = re.sub(r'LXL', 'XC', fixed)
-#     fixed = re.sub(r'CCCC', 'CD', fixed)
-#     fixed = re.sub(r'DCD', 'CM', fixed)
-#     return fixed
 
 def roman_to_int(roman):
     total_value = 0
     prev_value = 0
 
     for letter in reversed(roman):
-        # print(i, letter, reversed_roman[i-1])
-
-        # If this number is less than it's predescessor (reversed)
+        # If this number is >= it's predescessor (reversed)
         if value[letter] >= prev_value:
             total_value += value[letter]
         else:
             total_value -= value[letter]
 
         prev_value = value[letter]
-        print(letter, value[letter], total_value)
     return total_value
-# def roman_to_int(roman):
-#     reversed_roman = roman[::-1]
-#     cur_value = 0
-#     prev_value = None
-#     for i, letter in enumerate(reversed_roman):
-#         # print(i, letter, reversed_roman[i-1])
 
-#         # If this number is less than it's predescessor (reversed)
-#         if value[letter] < value[reversed_roman[i-1]]:
-#             cur_value -= value[letter]
-#         else:
-#             cur_value += value[letter]
-#         print(letter, value[letter], cur_value)
-#     return cur_value
+def int_to_roman(num):
+    pass
 
 def simplify_roman(original):
-    fixed = roman_to_int(original)
-    return fixed
+    return int_to_roman(roman_to_int(original))
 
 def test(original, expected):
     if (simplify_roman(original) == expected):
