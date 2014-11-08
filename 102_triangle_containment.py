c4f5f3e4e2ef73dc7d18cf0bd6879cd397558a8d
Paul Foster <paulfstr@gmail.com>
2014-11-08 22:40:41 +1100

    Solve problem 102

--- 102_triangle_containment.py
+++ 102_triangle_containment.py
@@ -1,19 +1,29 @@
-import re
-
-def main():
-    # triangles = re.findall(r'(-?\d+),(-?\d+),(-?\d+),(-?\d+),(-?\d+),(-?\d+)', open("p102_triangles.txt").read())
-    # print(int('-1'))
-    # return
+def read_triangles():
     triangles = []
     for line in open("p102_triangles.txt"):
         splits = [int(s) for s in line.split(',')]
+
+        # Split the line into three two-wide tuples representing the three
+        # coordinates
         coords = tuple(tuple(splits[i:i+2]) for i in range(0, len(splits), 2))
-        print(coords)
-        triangles.append((splits))
-        # (split[:2], split[2:])
-    # [tuple()
-    # coords = open("p102_triangles.txt").read().split(',')
-    # print(coords[:5])
+        triangles.append((coords))
+
+    return triangles
+
+def determinant(p1, p2, p3):
+    return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+def contains_origin(triangle):
+    origin = (0, 0)
+    det1 = determinant(origin, triangle[0], triangle[1]) < 0
+    det2 = determinant(origin, triangle[1], triangle[2]) < 0
+    det3 = determinant(origin, triangle[2], triangle[0]) < 0
+    return det1 == det2 == det3
+
+def main():
+    triangles = read_triangles()
+    answer = sum(1 for triangle in triangles if contains_origin(triangle))
+    print(answer)
 
 if __name__ == '__main__':
     main()
