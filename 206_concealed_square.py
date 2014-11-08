489938545900fcd7ea347be8c1ca692cf19d03dc
Paul Foster <paulfstr@gmail.com>
2014-11-08 16:33:10 +1100

    Complete problem 206

--- 206_concealed_square.py
+++ 206_concealed_square.py
@@ -1,20 +1,49 @@
-import re
+import itertools
+import math
 
 # The answer must lie between sqrt(1020304050607080900) = 1010101010.1 and sqrt(1929394959697989990) = 1389026623.11
 LOWER_BOUND = 1010101010
 UPPER_BOUND = 1389026623
 
+# 9_0
+# (a + b)^2 = a^2 + ab + b^2
+
+def is_square(num):
+    root = math.sqrt(num)
+    return root == int(root)
+
 def main():
-    # print(str(11223344556677889940))
-    # print('1.2.3.4.5.6.7.8.9.0')
-    # print(re.match('1.2.3.4.5.6.7.8.9.0', str(1122334455667788940)))
+    # print(is_square(25))
+    # print(is_square(24))
+    # for num in range(100):
+    #     print(num, num*num)
     # return
+    # for num in range(LOWER_BOUND, UPPER_BOUND+1):
+    #     print(num)
+    #     if re.match('1.2.3.4.5.6.7.8.9.0', str(num*num)):
+    #         answer = num
+    #         break
+
+    # for num in range(LOWER_BOUND, UPPER_BOUND+1):
+    #     print(num)
+    #     if re.match('1.2.3.4.5.6.7.8.9.0', str(num*num)):
+    #         answer = num
+    #         break
+
+    # Maybe would be quicker if reversed range
+    # Generate all the in-between numbers that go in the gaps
+    for in_betweens in range(10**11):
+        print(str(in_betweens).zfill(10))
+        # When splicing 1234567890 and the in-betweens, ensure that leading
+        # zeros are kept
+        candidate_square_list = list(zip("1234567890", str(in_betweens).zfill(10)))
+
+        # Flatten the list of tuples of strings into an int
+        candidate_square = int(''.join(itertools.chain(*candidate_square_list)))
 
 
-    for num in range(LOWER_BOUND, UPPER_BOUND+1):
-        print(num)
-        if re.match('1.2.3.4.5.6.7.8.9.0', str(num*num)):
-            answer = num
+        if is_square(candidate_square):
+            answer = math.sqrt(candidate_square)
             break
 
     print(answer)
---
+++ 206_concealed_square2.py
@@ -0,0 +1,22 @@
+# The answer must lie between
+#     sqrt(1020304050607080900) = 1010101010.1 and
+#     sqrt(1929394959697989990) = 1389026623.11
+# Furthermore, the answer must end in 30 or 70 as they are the only ending
+# digits that will result in a 0 at the end of the square
+LOWER_BOUND = 1010101000
+UPPER_BOUND = 1389026600
+
+def main():
+    # For every number within the bounds which ends in 30 or 70
+    for candidate in (hundreds + tens
+                      for hundreds in range(UPPER_BOUND, LOWER_BOUND, -100)
+                      for tens in (30, 70)):
+        # If it is in the form 1_2_3_4_5_6_7_8_9_0
+        if str(candidate*candidate)[::2] == "1234567890":
+            answer = candidate
+            break
+
+    print(answer)
+
+if __name__ == '__main__':
+    main()
