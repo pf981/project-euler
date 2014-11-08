edb2ad15c1a005b4e51d0c14e02d014e61eb6601
Paul Foster <paulfstr@gmail.com>
2014-11-09 01:27:57 +1100

    Tidy code in problem 072

--- 072_counting_fractions.py
+++ 072_counting_fractions.py
@@ -1,17 +1,4 @@
-# I started this problem by using fractions.Fraction to simplify each fraction
-# and put the in a set to remove duplicates. However, this method took over 6
-# minutes to run. By simply checking if the gcd is 1 and only accepting
-# simplified fractions, fractions.Fraction was avoided alltogether and the
-# runtime was reduced to 14s
-#
-# Instead of generating all fractions, it just looks at one fraction per
-# denominator. The numerator is constructed such that the fraction forms the
-# closest possible fraction to 1/2. The numerator is iterated down until the
-# fraction passes 1/2. The answer is the count of how many numbers lay between
-# 1/2 and 1/3
-# from fractions import Fraction
 import sympy
-import fractions
 
 # 1:
 # 2: 1/2
@@ -25,48 +12,24 @@ import fractions
 
 
 # MAX_DENOMINATOR = 1000000
-MAX_DENOMINATOR = 9
-# PRIMES = set(sympy.sieve.primerange(2, MAX_DENOMINATOR))
+MAX_DENOMINATOR = 8
 
-# Maybe I just want prime factors
-def non_trivial_divisors(n):
-    # All the divisors except 1 and n
-#    return sympy.ntheory.divisors(n)[1:-1]
-    return sympy.ntheory.primefactors(n)
+def extra_fractions_in_this_denominator(denominator):
+        extra_fractions = denominator - 1
+
+        # FIXME: This is excluding things multiple times
+        for divisor in sympy.ntheory.primefactors(denominator):
+            extra_fractions -= denominator // divisor - 1
+
+        return extra_fractions
 
 def main():
     answer = 0
     for denominator in range(2, MAX_DENOMINATOR + 1):
-
-        answer = 0 # FIXME: REMOVE
-        answer += denominator - 1
-
-        # This is excluding things multiple times
-        for divisor in non_trivial_divisors(denominator):
-            answer -= denominator // divisor - 1
-        # for divisor in non_trivial_divisors(denominator):
-        #     answer -= denominator // divisor - 1
-
-        print(denominator, answer)
-        # print(denominator, denominator - len(sympy.ntheory.divisors(denominator)) + 1)
-        # print(sympy.ntheory.divisors(denominator))
-        # print(non_trivial_divisors(denominator))
-        # print()
-        # answer += denominator - len(sympy.ntheory.divisors(denominator)) + 1
-        # print(denominator, denominator - len(sympy.ntheory.divisors(denominator)) + 1)
-        # print(sympy.ntheory.divisors(denominator))
-        # print(non_trivial_divisors(denominator))
-        # print()
-        # FIXME: Not divisors, common
-        # if denominator in PRIMES:
-        #     answer += denominator - 1
-        # else:
+        answer += extra_fractions_in_this_denominator(denominator)
 
         if denominator % 1000 == 0:
             print(denominator)
-        # for numerator in range(1, denominator):
-        #     if fractions.gcd(numerator, denominator) == 1:
-        #         answer += 1
 
     print(answer)
 
