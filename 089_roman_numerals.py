e27332757d67c0464503a95c56d1b7546c551f2e
Paul Foster <paulfstr@gmail.com>
2014-11-08 16:53:08 +1100

    Start problem 089

---
+++ 089_roman_numerals.py
@@ -0,0 +1,21 @@
+import re
+# 1000 M
+# 900 CM
+# 500 D
+# 400 CD
+# 100 C
+# 90 XC
+# 50 L
+# 40 XL
+# 10 X
+# 9 IX
+# 5 V
+# 4 IV
+# 1 I
+
+def main():
+    roman_numerals = re.findall("(\w+)", open("p089_roman.txt").read())
+    print(roman_numerals)
+
+if __name__ == '__main__':
+    main()
