5876b8cc57fa2039c280e6b15052c84bf8771484
Paul Foster <paulfstr@gmail.com>
2014-11-08 16:39:08 +1100

    Fix typo in comments in 206

--- 206_concealed_square2.py
+++ 206_concealed_square2.py
@@ -2,7 +2,7 @@
 #     sqrt(1020304050607080900) = 1010101010.1 and
 #     sqrt(1929394959697989990) = 1389026623.11
 # Furthermore, the answer must end in 30 or 70 as they are the only ending
-# digits that will result in a 0 at the end of the square
+# digits that will result in a 900 at the end of the square
 LOWER_BOUND = 1010101000
 UPPER_BOUND = 1389026600
 
