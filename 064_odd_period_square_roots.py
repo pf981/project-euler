343fe069c3cbba12f9f0e9e84763e20abb9e943e
Paul Foster <paulfstr@gmail.com>
2014-11-09 22:53:37 +1100

    Tidy up problem 064 - still produces incorrect answer.

--- 064_odd_period_square_roots.py
+++ 064_odd_period_square_roots.py
@@ -11,18 +11,21 @@ import math
 #     r_0 = x = sqrt(24)
 #     r_1 = 1 / (r_0 - a_0) = 1/(sqrt(24) - 4)
 #     a_1 = floor(r_1) = 1
-#     r_2 = 1 / (r_1 - a_1) = 1/
+#     etc.
 #
-# So the question is, how to (efficiently and difinitively) tell if it's periodic?
-# Well, we can see if r_i, a_i = r_k, a_k for i != k then it is periodic between i and k.
-#
-# From the mathworld page:
-#     The square root of a squarefree integer has a periodic continued fraction of the form
+# So the question is, how to (efficiently and difinitively) tell if it's
+# periodic?  Well, we can see if r_i, a_i = r_k, a_k for i != k then it is
+# periodic between i and k. But there is a better way. From the mathworld
+# page:
+#     The square root of a squarefree integer has a periodic continued
+#     fraction of the form:
 #         sqrt(n) = [a_0; (a_1, ..., a_n, 2a_0)]
-# So, we know that it must be periodic between a_0 and a_k such that a_k = 2a_0
+# So, we know that it must be periodic between a_0 and a_k such that a_k =
+# 2*a_0. That is, the period is k such that a_k = 2*a_0.
 MAX_ROOT = 13
 
 def main():
+    answer = 0
     for n in range(2, MAX_ROOT+1):
         x = math.sqrt(n)
 
@@ -35,7 +38,7 @@ def main():
 
         # r_0 = x
         r = [x]
-        # for _ in range(6):
+
         # x is periodic about a_0 and 2*a_0
         while a[-1] != 2 * a[0]:
             # r_n = 1 / (r_{n-1} - a_{n-1})
@@ -44,6 +47,15 @@ def main():
             # a_n = floor(r_n)
             a.append(math.floor(r[-1]))
         print("sqrt({0}) [{1}; {2}]".format(n, a[0], tuple(a[1:])))
+        # print("sqrt", n)
+
+        # Note that the periodic elements are a[1:]. a[0] is the first value
+        # that is not part of the period
+        # If the period is odd
+        if len(a[1:]) % 2 == 1:
+            answer += 1
+
+    print(answer)
 
 if __name__ == '__main__':
     main()
