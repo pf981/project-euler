9b74a1478d681a62506c00908793ed23388e5c1d
Paul Foster <paulfstr@gmail.com>
2014-11-10 03:05:40 +1100

    Use min_or_not_none rather than min in problem 082

--- 082_path_sum_three_ways.py
+++ 082_path_sum_three_ways.py
@@ -1,5 +1,8 @@
 import numpy as np
+from helpers import lists
 
+# This is just for testing if this solution works against the example. The
+# actual matrix is read in from a file
 MATRIX = np.array([[131, 673, 234, 103, 18],
                    [201, 96, 342, 965, 150],
                    [630, 803, 736, 422, 111],
@@ -12,24 +15,23 @@ def set_best_distance(row, col, best_distances, matrix):
         set_best_distance_no_up(down_row, col, best_distances, matrix)
 
     right = best_distances[row][col + 1]
-    up = best_distances[row-1][col] if row > 0 else 99999999999999 # FIXME: HACK
-    down = best_distances[row+1][col] if row < matrix.shape[0]-1 else 99999999999999 # FIXME: HACK
+    up = best_distances[row-1][col] if row > 0 else None
+    down = best_distances[row+1][col] if row < matrix.shape[0]-1 else None
 
     # The best distances to the right and up should already be known
-    best_distances[row][col] = matrix[row][col] + min(right, up, down)
+    best_distances[row][col] = matrix[row][col] + lists.min_of_not_none(right, up, down)
 
 def set_best_distance_no_up(row, col, best_distances, matrix):
-    # print("@", row, col)
     right = best_distances[row][col + 1]
-    down = best_distances[row+1][col] if row < matrix.shape[0]-1 else 99999999999999 # FIXME: HACK
+    down = best_distances[row+1][col] if row < matrix.shape[0]-1 else None
 
-    best_distances[row][col] = matrix[row][col] + min(right, down)
+    best_distances[row][col] = matrix[row][col] + lists.min_of_not_none(right, down)
 
 def length_of_shortest_path(matrix):
-    # 0 denotes that the best distance has not yet been computed
-    best_distances = np.zeros_like(matrix)
-    # best_distances = np.empty_like(matrix)
-    # best_distances.fill(np.nan)
+    # best_distances[row][col] is an integer that denotes the minimum distance
+    # from (row, col) to the far right column. The minimum path will just be
+    # the smallest value in the left-most column of best_distances
+    best_distances = np.empty_like(matrix)
 
     # Set the last column's best distance to the value of the matrix
     for row in range(matrix.shape[0]):
@@ -37,31 +39,13 @@ def length_of_shortest_path(matrix):
 
     # From the second-last column to the first column
     for col in reversed(range(matrix.shape[1]-1)):
+        # From the first row to the last row
         for row in range(matrix.shape[0]):
+            # Determine the length of the minimal path from (row, col) to the
+            # far right column
             set_best_distance(row, col, best_distances, matrix)
-            # print(row, col)
-            # print(best_distances)
-            # print()
-            # best_distances[row][col] = best_distance(row,
-            #                                                col,
-            #                                                best_distances,
-            #                                                matrix)
-            # best_distances[row][col] = np.nanmin([best_distance(row, # Right
-            #                                                           col + 1,
-            #                                                           best_distances,
-            #                                                           matrix),
-            #                                             best_distance(row + 1, # Down
-            #                                                           col,
-            #                                                           best_distances,
-            #                                                           matrix),
-            #                                             best_distance(row - 1, # Up
-            #                                                           col,
-            #                                                           best_distances,
-            #                                                           matrix)])
 
-    # print(best_distances)
-
-    # The shortest path is the minimum of the first column of best distances
+    # The shortest path is the minimum of the first column
     return min(best_distances[:,0])
 
 def main():
