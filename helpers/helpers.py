8979db4fcee1c703323856265b81243aad3ff777
Paul Foster <paulfstr@gmail.com>
2014-11-10 03:17:58 +1100

    Move min_of_not_none out of helpers.py

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -124,13 +124,3 @@ def squares_to(n):
 
 def is_palindrome(num):
     return str(num) == str(num)[::-1]
-
-
-# def min_of_not_none(*args):
-#     """
-#     Returns the minimum of the arguments, ignoring None values. If all are
-#     None, None will be returned
-#     """
-#     if all(element is None for element in args):
-#         return None
-#     return min(element for element in args if element != None)
