7b2b8efeb7a701951ad00f1811c810b196873d80
Paul Foster <paulfstr@gmail.com>
2014-11-11 10:59:07 +1100

    Fix ring generation in problem 068

--- 068_magic_5-gon_ring.py
+++ 068_magic_5-gon_ring.py
@@ -4,63 +4,54 @@ import itertools
 GONS = 3
 
 # The ring contains values 1 to MAX_VALUE
-MAX_VALUE = GONS * 3
+MAX_VALUE = GONS * 2
 
+# FIXME: These don't necessarily add to 9!!!
 def generate_all_triples():
     for first in range(1, MAX_VALUE+1):
-        for second in range(1, MAX_VALUE+1):
-            for third in range(1, MAX_VALUE+1):
+        for second in set(range(1, MAX_VALUE+1)) - set([first]):
+            for third in set(range(1, MAX_VALUE+1)) - set([first, second]):
                 if first + second + third == 9:
                     yield (first, second, third)
 
-# def candidate_ring(triples, num_lines):
-#     # if num_lines == 0:
-#     #     yield []
-#     if num_lines == 1:
-#         for triple in triples:
-#             yield [triple]
-
-#     for triple in triples:
-#         for others in candidate_ring(triples, num_lines-1):
-#             yield [triple] + others
-
-def candidate_ring(used_lines, triples, num_lines):
+# FIXME: Remove duplicates
+def generate_rings(used_lines, triples, num_lines):
     if num_lines == 0:
         yield used_lines
 
     for line in triples:
         # Check if this line will be valid if added
-        if line[0] in list(itertools.chain(*used_lines)):
-            continue
+        if used_lines:
+            # Ensure that the outer-most value is unique
+            if line[0] in list(itertools.chain(*used_lines)):
+                continue
 
-        for ring in candidate_ring(used_lines + [line], triples, num_lines-1):
+            # Ensure the middle value of this line is the last value of the
+            # previous line
+            if line[1] != used_lines[-1][2]:
+                continue
+
+            # Ensure the last value of this line is second value of the line two back
+            if len(used_lines) >= 2 and line[2] != used_lines[-2][1]:
+                continue
+
+            # Ensure the last value is not in the previous line
+            if line[2] in used_lines[-1]:
+                continue
+
+        # Recursively add all the next rings
+        for ring in generate_rings(used_lines + [line], triples, num_lines-1):
             yield ring
-        # yield candidate_ring(used_lines + [line], triples, num_lines-1)
-        # for others in candidate_ring(used_lines + [triple], triples, num_lines-1):
-        #     yield [line] + others
 
-def generate_rings():
+def main():
     # Generate all triples that add to 9
     triples = list(generate_all_triples())
 
-    for ring in candidate_ring([], triples, GONS):
-        print("@", ring)
-
-    # # For each set of three lines
-    # for first in triples:
-    #     for second in triples:
-    #         for third in triples:
-    #             # If the set forms a valid ring
-    #             # if first[0] not in list(second) + list(third) and \
-    #             #    second[0] not in list(first) + list(third) and \
-    #             #    third[0] not in list(first) + list(second):
-    #             #     print(first, second, third)
-    #             print(first, second, third)
-
-
-
-def main():
-    generate_rings()
+    for ring in generate_rings([], triples, GONS):
+        for line in ring:
+            print("{0},{1},{2};".format(*line), end="")
+        print()
+        # print(list(itertools.chain(*ring)))
     # answer = max(int(''.join(ring)) for ring in generate_rings())
     # print(answer)
 
