4e71977a75e89c06be5a72cb950bc8fe952ed3f1
Paul Foster <paulfstr@gmail.com>
2014-11-11 09:42:19 +1100

    Tidy problem 087

--- 087_prime_power_triples.py
+++ 087_prime_power_triples.py
@@ -3,28 +3,24 @@ import sympy
 MAX_NUM = 50000000
 
 def main():
-    print("Generating {0} primes.".format(MAX_NUM))
     primes = list(sympy.sieve.primerange(2, MAX_NUM**(1/2)))
     squares = [prime**2 for prime in primes]
     cubes = [prime**3 for prime in primes if prime**3 < MAX_NUM]
     fourths = [prime**4 for prime in primes if prime**4 < MAX_NUM]
-    print("Done.")
-    # print(squares)
-    # print(cubes)
-    # print(fourths)
-    # sums = [square + cube + fourth
-    #         for square in squares
-    #         for cube in cubes
-    #         for fourth in fourths
-    #         if square + cube + fourth < MAX_NUM]
-    # print(sums)
+
     sums = set()
+
+    # For all combinations of square prime, cube prime and fourth prime power
     for square in squares:
         print(square)
         for cube in cubes:
             for fourth in fourths:
+                # Compute the sum
                 prime_power_triple = square + cube + fourth
+
+                # If the sum is within the range we are looking
                 if prime_power_triple < MAX_NUM:
+                    # Store it and remove duplicates
                     sums.add(prime_power_triple)
 
     answer = len(sums)
