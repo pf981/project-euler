a7f44bee1baf382b753d3a7ec508ca9c34972a99
Paul Foster <paulfstr@gmail.com>
2014-11-12 07:02:21 +1100

    Simplify the implementation of problem 068v2 by initialising each ring in a single statement

--- 068_magic_5-gon_ring2.py
+++ 068_magic_5-gon_ring2.py
@@ -22,28 +22,12 @@ def generate_rings():
     """
     for perm in itertools.permutations(list(range(1,10))):
         # A ring consists of 5 lines. Each line has 3 values
-        ring = [[0]*3 for _ in range(5)]
-
         # For the number representation to be 16 digits, 10 must be on the outer edge
-        ring[0][0] = 10
-        ring[0][1] = perm[0]
-        ring[0][2] = perm[1]
-
-        ring[1][0] = perm[2]
-        ring[1][1] = perm[1]
-        ring[1][2] = perm[3]
-
-        ring[2][0] = perm[4]
-        ring[2][1] = perm[3]
-        ring[2][2] = perm[5]
-
-        ring[3][0] = perm[6]
-        ring[3][1] = perm[5]
-        ring[3][2] = perm[7]
-
-        ring[4][0] = perm[8]
-        ring[4][1] = perm[7]
-        ring[4][2] = perm[0]
+        ring = ((10, perm[0], perm[1]),
+                (perm[2], perm[1], perm[3]),
+                (perm[4], perm[3], perm[5]),
+                (perm[6], perm[5], perm[7]),
+                (perm[8], perm[7], perm[0]))
 
         # If all the lines of the ring sum to the same value
         if all(sum(line) == sum(ring[0]) for line in ring):
