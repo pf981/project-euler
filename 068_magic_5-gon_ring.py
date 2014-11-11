91d17a0fe0b192881cee4b1618fcc602fea17469
Paul Foster <paulfstr@gmail.com>
2014-11-11 12:08:18 +1100

    Complete problem 068

--- 068_magic_5-gon_ring.py
+++ 068_magic_5-gon_ring.py
@@ -15,35 +15,55 @@ def generate_all_triples(list_sum):
                 if first + second + third == list_sum:
                     yield (first, second, third)
 
+def is_valid_line(line, used_lines):
+    # If it is the first line, then it must be valid
+    if not used_lines:
+        return True
+
+    used_values_list = list(itertools.chain(*used_lines))
+
+    # Ensure that the outer-most value is unique
+    if line[0] in used_values_list:
+        return False
+
+    # Ensure the middle value of this line is the last value of the
+    # previous line
+    if line[1] != used_lines[-1][2]:
+        return False
+    elif used_values_list.count(line[1]) > 1:
+        return False
+
+    # FIXME: FIX BELOW, above should be fine
+    # Ensure the last value of this line is second value of the line two back
+    # if len(used_lines) >= GONS-1 and line[2] != used_lines[-GONS+1][1]:
+    #     return False
+    if len(used_lines) >= GONS-1:
+        if line[2] != used_lines[-GONS+1][1]:
+            return False
+        elif used_values_list.count(line[2]) > 1:
+            return False
+    elif used_values_list.count(line[2]) > 0:
+        return False
+
+
+    # Ensure the last value is not in any previous lines # FIXME: Except one of them
+    # Count how many times line[2] occurs in used_lines. If the length is big, it should be 1, if the length is small, it should be 0. FIXME: This isn't fixed yet
+    if any(line[2] in used_lines[i] for i in range(1, min(GONS-2, len(used_lines)))):
+    # if any(line[2] in used_lines[i] for i in range(1, GONS-2)):
+    # if line[2] in used_lines[-1]:
+        return False
+
+    return True
+
+
 def generate_lines(used_lines, triples, num_lines):
     if num_lines == 0:
         yield used_lines
 
     for line in triples:
         # Check if this line will be valid if added
-        if used_lines:
-            # Ensure that the outer-most value is unique
-            if line[0] in list(itertools.chain(*used_lines)):
-                continue
-
-            # Ensure the middle value of this line is the last value of the
-            # previous line
-            if line[1] != used_lines[-1][2]:
-                continue
-
-            # Ensure the last value of this line is second value of the line two back
-            # FIXME: I think this is good now
-            if len(used_lines) >= GONS-1 and line[2] != used_lines[-GONS+1][1]:
-            # if len(used_lines) >= 2 and line[2] != used_lines[-GONS+1][1]:
-            # if len(used_lines) >= 2 and line[2] != used_lines[-2][1]:
-                continue
-
-            # Ensure the last value is not in any previous lines # FIXME: Except one of them
-            # Count how many times line[2] occurs in used_lines. If the length is big, it should be 1, if the length is small, it should be 0. FIXME: This isn't fixed yet
-            if any(line[2] in used_lines[i] for i in range(1, min(GONS-2, len(used_lines)))):
-            # if any(line[2] in used_lines[i] for i in range(1, GONS-2)):
-            # if line[2] in used_lines[-1]:
-                continue
+        if not is_valid_line(line, used_lines):
+            continue
 
         # Recursively add all the next rings
         for ring in generate_lines(used_lines + [line], triples, num_lines-1):
@@ -70,19 +90,24 @@ def generate_rings():
     return rings
 
 def main():
-    # for ring in generate_rings():
-    #     for line in ring:
-    #         print("{0},{1},{2};".format(*line), end="")
-    #     print()
+    for ring in generate_rings():
+        for line in ring:
+            print("{0},{1},{2};".format(*line), end="")
+        print()
 
         # print(list(itertools.chain(*ring)))
     # answer = max(int(''.join(ring)) for ring in generate_rings())
     # answer = max(int(''.join(str(s) for s in ring)) for ring in generate_rings())
     # for ring in generate_rings():
         # print(int(''.join(str(n) for n in itertools.chain(*ring))))
-    answer = max(int(''.join(str(n) for n in itertools.chain(*ring)))
-                 for ring in generate_rings())
-
+    # answer = max(int(''.join(str(n) for n in itertools.chain(*ring)))
+    #              for ring in generate_rings())
+    answer = 0
+    for ring in generate_rings():
+        for n in itertools.chain(*ring):
+            chain_int = int(''.join(str(n) for n in itertools.chain(*ring)))
+            if len(str(chain_int)) == 16:
+                answer = max(answer, chain_int)
     # FIXME: Something is wrong with the validation. There should only be a maximum of two of any value, but this finds one with three tens
     print(answer)
 
